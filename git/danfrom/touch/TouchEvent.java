02af87c620bba3a065a437616cee093a7d13f864
FTI <dan.from@gmail.com>
2017-07-25 13:50:09 +0200

    Added locale coordinations
    Added locale coordinations to the event

--- git/danfrom/touch/TouchEvent.java
+++ git/danfrom/touch/TouchEvent.java
@@ -1,18 +1,19 @@
 package git.danfrom.touch;
 
-import java.awt.Point;
 import java.awt.geom.Point2D;
 
 public class TouchEvent {
   
   private int device;
   private int touchid;
-  private Point2D location;
+  private Point2D localeLocation;
+  private Point2D locationOnScreen;
   
-  public TouchEvent (int device, int touchid, Point2D location) {
+  public TouchEvent (int device, int touchid, Point2D localeLocation, Point2D locationOnScreen) {
     this.device = device;
     this.touchid = touchid;
-    this.location = location;
+    this.localeLocation = localeLocation;
+    this.locationOnScreen = locationOnScreen;
   }
   
   public Integer getDevice () {
@@ -23,8 +24,12 @@ public class TouchEvent {
     return this.touchid;
   }
   
+  public Point2D getLocaleLocation () {
+    return this.localeLocation;
+  }
+  
   public Point2D getLocationOnScreen () {
-    return this.location;
+    return this.locationOnScreen;
   }
   
 }
